690e5de644dda28fa6c6eec0eba09a5804795719
Analicesilva-27 <da.silva.analice@escola.pr.gov.br>
2025-11-04 08:38:16 -0300

    inicio

---
+++ js/js/principal.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-pasciente")
+
+vartdpeso = paciente.querySelector(".info-peso");
+var peso = tdpeso.textContent;
+
+vartdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
